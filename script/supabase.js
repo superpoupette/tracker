8eef62af33ad10234356987a4a240c499edb5024
superpoupette <excaaalibur@gmail.com>
2026-06-23 09:17:14 +0200

    Refactor Supabase client creation and logging
    Updated Supabase client initialization and improved error logging.

--- script/supabase.js
+++ script/supabase.js
@@ -1,24 +1,17 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
 const supabaseUrl = 'https://enmwwlovnzzmpmebgqri.supabase.co'
 const supabaseKey = 'sb_publishable_LNuAaplPR9o5lDQTuDE9iQ_52IEM05-'
 
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-)
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function chargerDanses() {
   const { data, error } = await supabase
     .from('Danse')
     .select('*')
 
-  if (error) {
-    console.error('Erreur Supabase :', error)
-    return
-  }
-
-  console.log('Données récupérées :', data)
+  console.log('data =', data)
+  console.log('error =', error)
 }
 
 chargerDanses()
-console.log(window)
-console.log(window.supabase)
