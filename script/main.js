ba3e7642cac653568b1d51fd19f327fdf8127ebb
superpoupette <excaaalibur@gmail.com>
2026-06-23 11:23:56 +0200

    chargement supabase

--- script/main.js
+++ script/main.js
@@ -1,18 +1,23 @@
-const mesChorees = await chargerDanses();
-console.log(mesChorees);
+import { chargerDanses } from './supabase.js'
 
-ChoreeEnCours=trouverChoreeEnCours();
-console.log(ChoreeEnCours);
-afficherChoreesEnCours(ChoreeEnCours);
+async function init() {
+  const mesChorees = await chargerDanses()
+  console.log(mesChorees)
 
+  const ChoreeEnCours = trouverChoreeEnCours()
+  console.log(ChoreeEnCours)
 
-let BouttonAjouterChoree = document.getElementById("BouttonAjouterChoree");
-BouttonAjouterChoree.addEventListener("click", () => { 
-    console.log("ok");
+  afficherChoreesEnCours(ChoreeEnCours)
+
+  const BouttonAjouterChoree = document.getElementById("BouttonAjouterChoree")
+
+  BouttonAjouterChoree.addEventListener("click", () => {
+    console.log("ok")
     ajouterChoree()
-});
+  })
 
-AfficherSemaineActuelle()
+  AfficherSemaineActuelle()
+}
 
-
+init()
 
--- script/supabase.js
+++ script/supabase.js
@@ -1,21 +1,21 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const supabaseUrl = 'https://enmwwlovnzzmpmebgqri.supabase.co'
-const supabaseKey = 'sb_publishable_LNuAaplPR9o5lDQTuDE9iQ_52IEM05-'
+const supabaseKey = '...'
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function chargerDanses() {
+export async function chargerDanses() {
   const { data, error } = await supabase
     .from('Danse')
     .select('*')
 
   if (error) {
-    console.error(error);
-    return [];
+    console.error(error)
+    return []
   }
 
-  return data;
+  return data
 }
 
 
